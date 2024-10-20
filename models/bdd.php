862dd0e0fd1c75125af1c8a80b361f60c4b3176b
Lord Erwan <erwan.a.marechal@gmail.com>
2024-10-20 16:01:21 +0200

    Refactor database setup: remove old bd.sql, update connection in bdd.php, and create new cave_a_vin.sql with correct schema

--- models/bdd.php
+++ models/bdd.php
@@ -8,7 +8,8 @@ class Bdd {
     public static function connexion() {
         if (self::$instance === null) {
             try {
-                self::$instance = new PDO('mysql:host=localhost;dbname=bd;charset=utf8', 'root', '');
+                // Remplacer 'bd' par le nom correct de la base de données
+                self::$instance = new PDO('mysql:host=localhost;dbname=cave_a_vin;charset=utf8', 'root', '');
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (Exception $e) {
                 die('Erreur : ' . $e->getMessage());
